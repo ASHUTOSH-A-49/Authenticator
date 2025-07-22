75100af42865ca2ac2509b00101b0d9072559d19
Ashutosh Behera <ashupoco66@gmail.com>
2025-07-22 16:36:05 +0530

    creating react app and setting up env variavbles ad installing req libraries

---
+++ src/app/login/page.tsx
@@ -0,0 +1,3 @@
+export default function LoginPage(){
+    return <div className="flex-wrap bg-amber-800"><h1 className="text-center text-white text-3xl">Login</h1></div>
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,3 @@
+export default function SignupPage(){
+        return <div className="flex-wrap bg-amber-800"><h1 className="text-center text-white text-3xl">Signup</h1></div>
+}
